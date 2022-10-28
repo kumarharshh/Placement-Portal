28c7d0522ab9787a6c4501e6b346aa276233ec92
kumarharshh <90533930+kumarharshh@users.noreply.github.com>
2022-10-28 22:58:54 +0530

    Add files via upload

---
+++ placement.js
@@ -0,0 +1,27 @@
+document.getElementById("handle").addEventListener("click",function(){
+     alert("You've Successfully Registered for this drive");
+     document.getElementById("handle").innerHTML="Registered";
+     document.getElementById("textchange").innerHTML="YES";
+     document.getElementById("textchange1").innerHTML="2401";
+});
+document.getElementById("handle1").addEventListener("click",function(){
+  alert("You've Successfully Registered for this drive");
+  document.getElementById("handle1").innerHTML="Registered";
+  document.getElementById("textchange2").innerHTML="YES";
+  document.getElementById("textchange3").innerHTML="3444";
+});
+document.getElementById("handle2").addEventListener("click",function(){
+  alert("You've Successfully Registered for this drive");
+  document.getElementById("handle2").innerHTML="Registered";
+  document.getElementById("textchange4").innerHTML="YES";
+  document.getElementById("textchange5").innerHTML="3444";
+});
+document.getElementById("handle3").addEventListener("click",function(){
+  alert("You've Successfully Registered for this drive");
+  document.getElementById("handle3").innerHTML="Registered";
+  document.getElementById("textchange6").innerHTML="YES";
+  document.getElementById("textchange7").innerHTML="3444";
+});
+document.getElementById("publishDrive11").addEventListener("click",function(){
+  alert("Successfully Registered For Placement Services");
+});
